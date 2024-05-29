e9f7ed46f79b0f4a062d40a4241eb01c2e365635
zarifRahman <zarif.shahriar.rahman@gmail.com>
2024-05-29 23:22:06 +0600

    add

--- api/app.js
+++ api/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json())
 
 app.use("/api/posts", postRoute);
+app.use(cookieParser());
+
 
 // Auth
 app.use("/api/auth", authRoute);
