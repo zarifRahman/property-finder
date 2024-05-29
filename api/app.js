2cafa047cf9053e6d8e4ed109f9ba777dd9c986f
zarifRahman <zarif.shahriar.rahman@gmail.com>
2024-05-29 17:58:43 +0600

    Update .gitignore to ignore node_modules and remove node_modules from tracking

--- api/app.js
+++ api/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 const app = express();
 
+app.use('/api/test', (req, res) => {
+	res.send("it works")
+})
+
 app.listen(8800, () => {
-  console.log("Server is running!");
+  console.log("Server is running hh!");
 });
